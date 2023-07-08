223739b0f39ba3f2b9db061755972fb604adf90e
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-08 18:15:35 -0300

    change color in my home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
+import Home from './components/Home'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Nav />
+        <Home />
         <Footer />
       </BrowserRouter>
     </div>

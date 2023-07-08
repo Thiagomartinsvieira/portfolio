b1d3e0aec1230a17b63af95a38d40b74dd76dbd5
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-08 18:13:38 -0300

    add linkedin icon

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <footer id="footer">
-            <a href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/"><img src="https://i.imgur.com/pxYs2py.png" alt="Linkedin" /></a>
+            <a href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/pxYs2py.png" alt="Linkedin" /></a>
         </footer>
     )
 }

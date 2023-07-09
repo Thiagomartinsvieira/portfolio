c1767d211b08127cdcf770b5e8195935f16fb2ac
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-09 20:25:06 -0300

    layout improvements

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -21,11 +21,11 @@ const Home = () => {
             <div className='social-media'>
                 <a href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/"
                 target='_blank' rel="noreferrer">
-                    <FaLinkedin size={30}/>
+                    <FaLinkedin size={40}/>
                 </a>
                 <a href="https://github.com/Thiagomartinsvieira"
                 target='_blank' rel="noreferrer">
-                    <FaGithub size={30}/>
+                    <FaGithub size={40}/>
                 </a>
             </div>   
 
@@ -33,11 +33,11 @@ const Home = () => {
                 <p>Tech Stack</p>
                 <div className='logos'>
                     <ul>
-                        <li><SiHtml5 size={35} color='#2d2e32'/></li>
-                        <li><SiCss3 size={35} color='#2d2e32'/></li>
-                        <li><SiJavascript  size={35} color='#2d2e32' /></li>
-                        <li><SiTypescript size={35} color='#2d2e32'/></li>      
-                        <li><SiReact size={35} color='#2d2e32'/></li> 
+                        <li><SiHtml5 size={40} color='#E34F26'/></li>
+                        <li><SiCss3 size={40} color='#1572B6'/></li>
+                        <li><SiJavascript  size={40} color='#F7DF1E' /></li>
+                        <li><SiTypescript size={40} color='#007ACC'/></li>      
+                        <li><SiReact size={40} color='#61DAFB'/></li> 
                     </ul>
                 </div>
             </div>

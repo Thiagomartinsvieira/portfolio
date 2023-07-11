47e1d69276199ac3ff632172d49e04cc9375ef20
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-11 20:36:27 -0300

    added component projects

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,17 @@
+import './About.css';
+import { } from 'react-icons/'
+
+const Projects = () => {
+    return (
+        <div>
+            <section className="projects-content">
+                <h2>Projects</h2>                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis reiciendis consequuntur ratione? Reiciendis quis non iusto molestias adipisci assumenda ipsam ipsa culpa. Sed, iusto quis repellendus ad tempora praesentium.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis reiciendis consequuntur ratione? Reiciendis quis non iusto molestias adipisci assumenda ipsam ipsa culpa. Sed, iusto quis repellendus ad tempora praesentium.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae perferendis reiciendis consequuntur ratione? Reiciendis quis non iusto molestias adipisci assumenda ipsam ipsa culpa. Sed, iusto quis repellendus ad tempora praesentium.</p>
+            </section>
+        </div>
+    );
+}
+
+export default Projects;

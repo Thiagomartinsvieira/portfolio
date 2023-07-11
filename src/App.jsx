62946883491e5112c8a848b5085452dae06566b0
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-11 20:39:00 -0300

    add to portfolio projects, contact, footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
         <Nav />
         <Home />
         <About />
-        
+        <Projects />
+        <Contact />
+        <Footer />
       </BrowserRouter>
     </div>
   )

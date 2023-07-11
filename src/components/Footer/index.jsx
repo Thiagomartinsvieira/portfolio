01d16c573c760605a02abbe5b94cebba01620856
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-11 20:40:14 -0300

    finished v1 of component footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,22 @@
 import "./Footer.css"
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
     return (
         <footer id="footer">
-            <a href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/" target="_blank" rel="noreferrer"><img src="https://i.imgur.com/pxYs2py.png" alt="Linkedin" /></a>
+            <div className="container">
+                <div className="footerrc">
+                <h3>Copyright © 2023. All rights are reserved</h3>
+                </div>
+                <div className="footerrc-social">
+                    <a href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/">
+                        <FaLinkedin size={35} color="white"/>
+                    </a>
+                    <a href="https://github.com/Thiagomartinsvieira">
+                        <FaGithub size={35} color="white"/>
+                    </a>
+                </div>
+            </div>
         </footer>
     )
 }

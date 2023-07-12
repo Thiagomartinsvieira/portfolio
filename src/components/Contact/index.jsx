14364c5a1b8fb7537193926e4431255bdb4145e6
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-12 20:51:30 -0300

    aligning elements by column

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,29 +6,27 @@ const Contact = () => {
         <div>
             <section id='contact' className="contact">
                 <div className='contatct-content'>
-                <div className='container'>
-                    <div className='contact-title'>
-                        <h4>Contact</h4>
-                        <h3>Let's talk! </h3>
-                        <p> As a front-end developer, I am always open to new contact opportunities. If you're looking for a qualified professional with a passion for creating amazing user interfaces, I'm available to collaborate on challenging projects. Please feel free to contact me to discuss ideas, job opportunities, or any queries related to front-end development. I look forward to working in partnership and contributing to the success of new projects. 🙂</p>
+                    <div className='container'>
+                        <div className='contact-title'>
+                            <h4>Contact</h4>
+                            <h3>Let's talk! </h3>
+                            <p> As a front-end developer, I am always open to new contact opportunities. If you're looking for a qualified professional with a passion for creating amazing user interfaces, I'm available to collaborate on challenging projects. Please feel free to contact me to discuss ideas, job opportunities, or any queries related to front-end development. I look forward to working in partnership and contributing to the success of new projects. 🙂</p>
+                        </div>
+
                     </div>
-                  
-                </div>
-                <div className='contact-icons'>
-                    <div className='contact-icons-box'>
+
                     <div className='contact-info'>
-                        <h3>Location</h3>
+                        <h3>Location 📍</h3>
                         <p>São Paulo, Brazil</p>
                     </div>
-                    </div>
-                    <div className='contact-icon-box'>
-                        <div className='contact-info'>
-                            <h3>Mail</h3>
-                            <a href="mailto:thiagoalagadico@hotmail.com">thiagoalagadico@hotmail.com</a>
-                        </div>
-                    </div>
-                    </div>
                 </div>
+
+                <div className='contact-info'>
+                    <h3>Mail 📧 </h3>
+                    <a href="mailto:thiagoalagadico@hotmail.com">thiagoalagadico@hotmail.com</a>
+                </div>
+
+
             </section>
         </div>
     );

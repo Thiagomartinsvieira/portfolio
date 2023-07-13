d7025173ea0065dd21bbe85b1dbf79e5e4e1cc4c
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-13 19:55:55 -0300

    formated by prettier

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Nav />
-                <Home />
-                <About />
-                <Projects />
-                <Contact />
-                <Footer />
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App

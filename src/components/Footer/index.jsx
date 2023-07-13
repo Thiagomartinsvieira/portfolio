90d94ee48278dce0124c438fa7099df5a563af31
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-13 19:58:49 -0300

    formated by prettier

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,24 +1,30 @@
-import "./Footer.css"
+import './Footer.css'
 import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
-const Footer = () => {
-    return (
-        <footer id="footer">
-            <div className="container">
-                <div className="footerrc">
-                <h3>Copyright © 2023. All rights are reserved</h3>
-                </div>
-                <div className="footerrc-social">
-                    <a href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/">
-                        <FaLinkedin size={35} color="white"/>
-                    </a>
-                    <a href="https://github.com/Thiagomartinsvieira">
-                        <FaGithub size={35} color="white"/>
-                    </a>
-                </div>
-            </div>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer id="footer">
+      <div className="container">
+        <div className="footerrc">
+          <h3>Copyright © 2023. All rights are reserved</h3>
+        </div>
+        <div className="footerrc-social">
+          <a
+            href="https://www.linkedin.com/in/thiago-martins-vieira-074550192/"
+            className="social-icon"
+          >
+            <FaLinkedin size={35} color="white" />
+          </a>
+          <a
+            href="https://github.com/Thiagomartinsvieira"
+            className="social-icon"
+          >
+            <FaGithub size={35} color="white" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer

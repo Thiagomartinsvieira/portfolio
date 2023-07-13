09be21c8146247fbbc3d1452e2d25a6b9efbe064
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-13 19:56:57 -0300

    added first project for testing

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,43 +1,24 @@
+import React from 'react'
 import './projects.css'
 
 function Projects() {
-    return (
-        <div>
-            <section id="project">
+  return (
+    <div>
+      <section id="project">
+        <h1 className='projects-title'>Projetos</h1>
 
-                <h1>Projetos</h1>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad,
-                    temporibus. Soluta dignissimos atque beatae, aut fugit inventore
-                    eveniet commodi pariatur delectus debitis dicta vel, ullam maxime, hic
-                    sint. Debitis, reprehenderit.
-                </p>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad,
-                    temporibus. Soluta dignissimos atque beatae, aut fugit inventore
-                    eveniet commodi pariatur delectus debitis dicta vel, ullam maxime, hic
-                    sint. Debitis, reprehenderit.
-                </p>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad,
-                    temporibus. Soluta dignissimos atque beatae, aut fugit inventore
-                    eveniet commodi pariatur delectus debitis dicta vel, ullam maxime, hic
-                    sint. Debitis, reprehenderit.
-                </p>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad,
-                    temporibus. Soluta dignissimos atque beatae, aut fugit inventore
-                    eveniet commodi pariatur delectus debitis dicta vel, ullam maxime, hic
-                    sint. Debitis, reprehenderit.
-                </p>
-                
-            </section>
+        <div className="img-projects">
+          <img src="src/assets/prime-flix.png" alt="prime-flix" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            itaque saepe neque sapiente distinctio laborum aliquid, totam
+            necessitatibus quisquam corrupti asperiores ab quasi beatae dolorem
+            similique dolore deleniti sed porro.
+          </p>
         </div>
-    )
+      </section>
+    </div>
+  )
 }
 
 export default Projects

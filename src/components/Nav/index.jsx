4dab52ee217bf9f5b306f32204231b8350dc1827
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-14 21:08:53 -0300

    added classes for testing with responsive menu

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,12 +1,22 @@
 import { useEffect } from 'react'
 import './Nav.css'
 
+const menuToggle = document.querySelector('.menu-toggle')
+const menuList = document.querySelector('.menu-list')
+
+menuToggle.addEventListener('click', function () {
+  menuList.classList.toggle('active')
+})
+
 function Nav() {
-  useEffect(() => {}, [])
+  useEffect(() => { }, [])
 
   return (
-    <nav className="navMenu fixed">
-      <ul>
+    <nav className="navMenu fixed menu-hamburguer">
+      <button className="menu-toggle">
+        <span className="hamburguer-icon" />
+      </button>
+      <ul className="menu-list">
         <li>
           <a href="#home">Home</a>
         </li>

c821e81bc14356a00cc870beed50a36040fd614b
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-14 21:10:10 -0300

    adding project with view layout

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,20 +1,209 @@
 import React from 'react'
 import './projects.css'
+import { SiGithub, SiNetlify, SiReact, SiWeb3Dotjs } from 'react-icons/si'
 
 function Projects() {
   return (
     <div>
-      <section id="project">
-        <h1 className='projects-title'>Projetos</h1>
+      <section id="projects" className="projects-section">
+        <div className="projects-container">
+          <div className="projects-content">
+            <p className="projects-subtitle">Portfolio</p>
+            <h3 className="projects-title">
+              Each project is a unique piece of development 🧩
+            </h3>
+            <div className="projects-grid">
+              <div className="project-card project-card-1">
+                <div className="project-image">
+                  <a target="_blank" href="#" rel="noreferrer">
+                    <img src="src/assets/prime-flix.png" alt="website" />
+                  </a>
+                </div>
+                <div className="project-details">
+                  <h3 className="project-name">Prime Flix</h3>
+                  <p className="project-description">
+                    "Lorem ipsum dolor sit amet. Aut repudiandae atque ea
+                    exercitationem accusamus non modi dolores! Et labore
+                    officiis et libero temporibus aut voluptatum tempore. In
+                    obcaecati dolore aut iure dolores cum repellat beatae qui
+                    natus sunt."
+                  </p>
+                  <div className="project-stack">
+                    <p>
+                      <SiReact size={30} color="#61DAFB" />
+                    </p>
+                  </div>
+                  <div className="project-links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                      code <SiGithub />
+                    </a>
+                    <a target="_blank" href="#" rel="noreferrer">
+                      Deploy <SiNetlify color="#6bd1d7" />
+                    </a>
+                  </div>
+                </div>
+              </div>
 
-        <div className="img-projects">
-          <img src="src/assets/prime-flix.png" alt="prime-flix" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-            itaque saepe neque sapiente distinctio laborum aliquid, totam
-            necessitatibus quisquam corrupti asperiores ab quasi beatae dolorem
-            similique dolore deleniti sed porro.
-          </p>
+              <div className="project-card project-card-1">
+                <div className="project-image">
+                  <a target="_blank" href="#" rel="noreferrer">
+                    <img src="src/assets/prime-flix.png" alt="website" />
+                  </a>
+                </div>
+                <div className="project-details">
+                  <h3 className="project-name">Prime Flix</h3>
+                  <p className="project-description">
+                    "Lorem ipsum dolor sit amet. Aut repudiandae atque ea
+                    exercitationem accusamus non modi dolores! Et labore
+                    officiis et libero temporibus aut voluptatum tempore. In
+                    obcaecati dolore aut iure dolores cum repellat beatae qui
+                    natus sunt."
+                  </p>
+                  <div className="project-stack">
+                    <p>
+                      <SiReact size={30} color="#61DAFB" />
+                    </p>
+                  </div>
+                  <div className="project-links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                      code <SiGithub />
+                    </a>
+                    <a target="_blank" href="#" rel="noreferrer">
+                      Deploy <SiNetlify color="#6bd1d7" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="project-card project-card-1">
+                <div className="project-image">
+                  <a target="_blank" href="#" rel="noreferrer">
+                    <img src="src/assets/prime-flix.png" alt="website" />
+                  </a>
+                </div>
+                <div className="project-details">
+                  <h3 className="project-name">Prime Flix</h3>
+                  <p className="project-description">
+                    "Lorem ipsum dolor sit amet. Aut repudiandae atque ea
+                    exercitationem accusamus non modi dolores! Et labore
+                    officiis et libero temporibus aut voluptatum tempore. In
+                    obcaecati dolore aut iure dolores cum repellat beatae qui
+                    natus sunt."
+                  </p>
+                  <div className="project-stack">
+                    <p>
+                      <SiReact size={30} color="#61DAFB" />
+                    </p>
+                  </div>
+                  <div className="project-links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                      code <SiGithub />
+                    </a>
+                    <a target="_blank" href="#" rel="noreferrer">
+                      Deploy <SiNetlify color="#6bd1d7" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="project-card project-card-1">
+                <div className="project-image">
+                  <a target="_blank" href="#" rel="noreferrer">
+                    <img src="src/assets/prime-flix.png" alt="website" />
+                  </a>
+                </div>
+                <div className="project-details">
+                  <h3 className="project-name">Prime Flix</h3>
+                  <p className="project-description">
+                    "Lorem ipsum dolor sit amet. Aut repudiandae atque ea
+                    exercitationem accusamus non modi dolores! Et labore
+                    officiis et libero temporibus aut voluptatum tempore. In
+                    obcaecati dolore aut iure dolores cum repellat beatae qui
+                    natus sunt."
+                  </p>
+                  <div className="project-stack">
+                    <p>
+                      <SiReact size={30} color="#61DAFB" />
+                    </p>
+                  </div>
+                  <div className="project-links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                      code <SiGithub />
+                    </a>
+                    <a target="_blank" href="#" rel="noreferrer">
+                      Deploy <SiNetlify color="#6bd1d7" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="project-card project-card-1">
+                <div className="project-image">
+                  <a target="_blank" href="#" rel="noreferrer">
+                    <img src="src/assets/prime-flix.png" alt="website" />
+                  </a>
+                </div>
+                <div className="project-details">
+                  <h3 className="project-name">Prime Flix</h3>
+                  <p className="project-description">
+                    "Lorem ipsum dolor sit amet. Aut repudiandae atque ea
+                    exercitationem accusamus non modi dolores! Et labore
+                    officiis et libero temporibus aut voluptatum tempore. In
+                    obcaecati dolore aut iure dolores cum repellat beatae qui
+                    natus sunt."
+                  </p>
+                  <div className="project-stack">
+                    <p>
+                      <SiReact size={30} color="#61DAFB" />
+                    </p>
+                  </div>
+                  <div className="project-links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                      code <SiGithub />
+                    </a>
+                    <a target="_blank" href="#" rel="noreferrer">
+                      Deploy <SiNetlify color="#6bd1d7" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="project-card project-card-1">
+                <div className="project-image">
+                  <a target="_blank" href="#" rel="noreferrer">
+                    <img src="src/assets/prime-flix.png" alt="website" />
+                  </a>
+                </div>
+                <div className="project-details">
+                  <h3 className="project-name">Prime Flix</h3>
+                  <p className="project-description">
+                    "Lorem ipsum dolor sit amet. Aut repudiandae atque ea
+                    exercitationem accusamus non modi dolores! Et labore
+                    officiis et libero temporibus aut voluptatum tempore. In
+                    obcaecati dolore aut iure dolores cum repellat beatae qui
+                    natus sunt."
+                  </p>
+                  <div className="project-stack">
+                    <p>
+                      <SiReact size={30} color="#61DAFB" />
+                    </p>
+                  </div>
+                  <div className="project-links">
+                    <a target="_blank" href="#" rel="noreferrer">
+                      code <SiGithub />
+                    </a>
+                    <a target="_blank" href="#" rel="noreferrer">
+                      Deploy <SiNetlify color="#6bd1d7" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+
+
+            </div>
+          </div>
         </div>
       </section>
     </div>

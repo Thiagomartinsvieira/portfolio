b0712f3ef735d97354396ca41a5eb405313dd7aa
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-08-12 18:06:24 -0300

    add button to add dark mode

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,47 +1,71 @@
-import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
-import './Nav.css';
+import React, { useState } from 'react'
+import { FaBars, FaMoon, FaSun } from 'react-icons/fa'
+import './Nav.css'
+
+function MenuItem({ href, onClick, label }) {
+  return (
+    <li>
+      <a href={href} onClick={onClick}>
+        {label}
+      </a>
+    </li>
+  )
+}
 
 function Nav() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    setIsMenuOpen(!isMenuOpen)
+  }
 
   const handleMenuLinkClick = () => {
-    setIsMenuOpen(false);
-  };
+    setIsMenuOpen(false)
+  }
+
+  const handleThemeToggle = () => {
+    setIsDarkTheme(!isDarkTheme)
+  }
 
   return (
     <nav className="navbar">
-      <button className={`menu-toggle ${isMenuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
-        <FaBars size={28} />
-      </button>
+      <div className="buttons-container">
+        <button
+          className={`menu-toggle ${isMenuOpen ? 'open' : ''}`}
+          onClick={handleMenuToggle}
+        >
+          <FaBars size={28} />
+        </button>
+      </div>
       <ul className={`menu-list ${isMenuOpen ? 'open' : ''}`}>
-        <li>
-          <a href="#home" onClick={handleMenuLinkClick}>
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="#about" onClick={handleMenuLinkClick}>
-            About Me
-          </a>
-        </li>
-        <li>
-          <a href="#projects" onClick={handleMenuLinkClick}>
-            Projects
-          </a>
-        </li>
-        <li>
-          <a href="#contact" onClick={handleMenuLinkClick}>
-            Contact
-          </a>
-        </li>
+        <MenuItem href="#home" onClick={handleMenuLinkClick} label="Home" />
+        <MenuItem
+          href="#about"
+          onClick={handleMenuLinkClick}
+          label="About Me"
+        />
+        <MenuItem
+          href="#projects"
+          onClick={handleMenuLinkClick}
+          label="Projects"
+        />
+        <MenuItem
+          href="#contact"
+          onClick={handleMenuLinkClick}
+          label="Contact"
+        />
+        <div className="theme-toggle-container">
+          <button
+            onClick={handleThemeToggle}
+            className="darkmode theme-toggle-button"
+          >
+            {isDarkTheme ? <FaSun size={28} /> : <FaMoon size={28} />}
+          </button>
+        </div>
       </ul>
     </nav>
-  );
+  )
 }
 
-export default Nav;
+export default Nav

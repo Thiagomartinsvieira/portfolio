0f0746e02066c3dbf2c00622a975ebe13f316836
Thiago Martins Vieira <thiago.devstack@gmail.com>
2025-11-23 21:36:36 -0300

    feat: update About me section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,11 +9,15 @@ function About() {
         <h2 className="about-me">About Me</h2>
 
         <p className="text-side-p">
-        Hello, I'm Thiago Martins, a Full Stack Developer with two years of experience building dynamic and responsive applications. I specialize in React, Next.js, JavaScript, TypeScript, and Tailwind CSS, with back-end expertise in Node.js, PHP, and Java (Spring Boot).  
+      Full Stack Developer with experience in developing modern front-end and back-end solutions. On the front-end, I work with React, Next.js, JavaScript, TypeScript, Angular, Vue.js, and Tailwind CSS, creating responsive and high-performance interfaces.
 
-Currently, I work as a Web Developer at Smart Ideias e Soluções Digitais, creating tailored digital solutions. I also have experience managing databases (MySQL, PostgreSQL, MongoDB), hosting, and performance optimization with WordPress and Cloudflare.  
+Currently, I work as a Front-end Developer at ComHub, where I contribute to building applications using Next.js, React.js, TypeScript, and Tailwind CSS. I have experience consuming REST APIs, integrating with WebSocket, using libraries like Axios, and applying best development practices to deliver modern and scalable experiences.
 
-Passionate about technology and continuous learning, I’m open to opportunities to contribute to innovative projects and grow professionally.  
+On the back-end, I have knowledge of Node.js, Express, PHP, and Java with Spring Boot, as well as familiarity with databases such as PostgreSQL, MongoDB, and MySQL.
+
+I also have experience with WordPress, including theme and plugin customization, as well as knowledge of hosting, domain configuration, and optimization with Cloudflare.
+
+Proficient in Linux, Git, and collaboration tools, I am always seeking new challenges, continuous improvement, and opportunities that allow me to grow professionally and contribute innovative solutions.
 
 Thank you for your attention!
         </p>
